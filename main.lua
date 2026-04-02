bd804d285ed09490fc9f95170551c9a5d8b07e0b
amerameryou1-blip <amerameryou1@gmail.com>
2026-04-02 17:32:30 +0200

    update main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+-- Aurora Click Recorder v6.0
+-- Paste this ONE line into your executor:
+loadstring(game:HttpGet("https://raw.githubusercontent.com/amerameryou1-blip/Aurora1/main/ClickRecorder/Loader.lua"))()
